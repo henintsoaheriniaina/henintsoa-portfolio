75e3d968d9e0b45b1226b6bcdd2cb5b8f8399459
henintsoaheriniaina <belouhtsoa@gmail.com>
2025-02-03 10:48:43 +0300

    add header and add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="bg-red-400">App</div>;
+  const [isDark, setIsDark] = useState(false);
+  const toggleIsDark = () => {
+    setIsDark((p) => !p);
+  };
+  return (
+    <div
+      className={`${
+        isDark && "dark"
+      } dark:bg-neutral-800 dark:text-neutral-50 bg-neutral-50 min-h-screen w-full  space-y-4`}
+    >
+      <Navbar isDark={isDark} toggleIsDark={toggleIsDark} />
+      <HeroSection />
+      <section className="section" id="skills">
+        Compétence
+      </section>
+      <section className="section" id="projects">
+        Projets
+      </section>
+      <section className="section" id="about">
+        A propos
+      </section>
+    </div>
+  );
 };
 export default App;
